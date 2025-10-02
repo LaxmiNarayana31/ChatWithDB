f77439071eb053c7abcd01d3378f28bee55554dd
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2025-10-03 00:08:02 +0530

    Refactor main.py to improve schema folder setup and organization

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# main.py
+import os
 from app.streamlit import streamlit_app
+
+# Folder to store schema files
+SCHEMA_FOLDER = os.path.join(os.getcwd(), "schema")
+os.makedirs(SCHEMA_FOLDER, exist_ok=True)
 
 if __name__ == "__main__":
     streamlit_app.main()
